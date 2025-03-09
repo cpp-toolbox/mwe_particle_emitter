c6f7832ed54e750f9737e3e61485fb5d93bf1420
cuppajoeman <ccn@cuppajoeman.com>
2025-03-09 17:15:28 -0400

    feat: demo moving particle emitter

--- src/graphics/batcher_x_particle_emitter_logic/batcher_x_particle_emitter_logic.cpp
+++
@@ -1,2 +0,0 @@
-#include "batcher_x_particle_emitter_logic.hpp"
-
--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #include <stb_image.h>
 #include <stb_image_write.h>
 
+#include "graphics/smoke_particle_emitter/smoke_particle_emitter.hpp"
 #include "graphics/particle_emitter/particle_emitter.hpp"
 #include "graphics/vertex_geometry/vertex_geometry.hpp"
 #include "graphics/texture_packer/texture_packer.hpp"
@@ -25,70 +26,16 @@
 
 #include <filesystem>
 
-class SmokeParticleEmitter {
-  public:
-    ParticleEmitter particle_emitter;
+glm::vec3 get_circular_position_3d(float radius, float centerX, float centerY, float centerZ) {
+    float time = (float)glfwGetTime();
+    float angle = time;
 
-    SmokeParticleEmitter(std::function<void(int)> on_particle_spawn_callback,
-                         std::function<void(int)> on_particle_death_callback)
-        : particle_emitter(life_span_lambda(), initial_velocity_lambda(), velocity_change_lambda(), scaling_lambda(),
-                           rotation_lambda(), spawn_delay_lambda(), on_particle_spawn_callback,
-                           on_particle_death_callback) {}
+    float x = centerX + radius * cos(angle);
+    float y = 0;
+    float z = centerY + radius * sin(angle);
 
-  private:
-    static std::function<float()> life_span_lambda() {
-        return []() -> float {
-            static std::mt19937 rng(std::random_device{}());
-            std::uniform_real_distribution<float> dist(1.0f, 3.0f);
-            return dist(rng);
-        };
-    }
-
-    static std::function<glm::vec3()> initial_velocity_lambda() {
-        return []() -> glm::vec3 {
-            static std::mt19937 rng(std::random_device{}());
-            std::uniform_real_distribution<float> horizontal_dist(-0.5f, 0.5f); // Small lateral variance
-            std::uniform_real_distribution<float> upward_dist(1.0f, 2.0f);      // Strong upward push
-
-            // Initial upward push with slight lateral drift
-            float dx = horizontal_dist(rng);
-            float dy = upward_dist(rng); // Main upward velocity
-            float dz = horizontal_dist(rng);
-
-            return glm::vec3(dx, dy, dz);
-            /*return glm::vec3(0, 0, 0);*/
-        };
-    }
-
-    static std::function<glm::vec3(float, float)> velocity_change_lambda() {
-        return [](float life_percentage, float delta_time) -> glm::vec3 {
-            static std::mt19937 rng(std::random_device{}());
-            std::uniform_real_distribution<float> horizontal_dist(-0.010f, 0.010f); // Small lateral variance
-            std::uniform_real_distribution<float> vertical_dist(0.2f, 0.3f);
-
-            float accel_x = horizontal_dist(rng);
-            float accel_y = vertical_dist(rng);
-            float accel_z = horizontal_dist(rng);
-
-            glm::vec3 smoke_push_down = -glm::vec3(accel_x, accel_y, accel_z) * delta_time;
-            return smoke_push_down;
-        };
-    }
-
-    static std::function<float(float)> scaling_lambda() {
-        return [](float life_percentage) -> float { return std::max(life_percentage * 2.0f, 0.0f); };
-    }
-
-    static std::function<float(float)> rotation_lambda() {
-        return [](float life_percentage) -> float { return life_percentage / 5.0f; };
-    }
-
-    static std::function<float()> spawn_delay_lambda() {
-        return []() -> float {
-            return 0.01f; // Spawn a new particle every 0.05 seconds
-        };
-    }
-};
+    return glm::vec3(x, y, z);
+}
 
 int main() {
     auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
@@ -174,6 +121,7 @@ int main() {
         shader_cache.set_uniform(ShaderType::TEXTURE_PACKER_CWL_V_TRANSFORMATION_UBOS_1024,
                                  ShaderUniformVariable::WORLD_TO_CAMERA, view);
 
+        spe.particle_emitter.transform.position = get_circular_position_3d(2, 0, 0, 0);
         batcher_x_particle_emitter_logic.draw_particles(spe.particle_emitter, fps_camera, delta_time, projection, view);
 
         // load in the matrices
